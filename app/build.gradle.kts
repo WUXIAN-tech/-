8d4017fc6bfadd115445115c2270dad29058bc23
WUXIAN科技 <923383218@qq.com>
2026-06-21 15:54:45 +0800

    fix: 加全量 META-INF 排除规则，关闭 lint 中止

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,8 +47,22 @@ android {
 
     packaging {
         resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+            excludes += setOf(
+                "/META-INF/{AL2.0,LGPL2.1}",
+                "/META-INF/DEPENDENCIES",
+                "/META-INF/LICENSE",
+                "/META-INF/LICENSE.txt",
+                "/META-INF/NOTICE",
+                "/META-INF/NOTICE.txt",
+                "/META-INF/MANIFEST.MF",
+                "/META-INF/*.kotlin_module",
+            )
         }
+    }
+
+    lint {
+        abortOnError = false
+        checkReleaseBuilds = false
     }
 }
 
